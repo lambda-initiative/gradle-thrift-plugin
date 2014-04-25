260cdaa01975a17ff55cd59e71a6a534c86171a1
zfy0701 <zfy0701@gmail.com>
2014-04-24 23:43:23 -0700

    Put annotation back to support incrementality

--- src/main/groovy/com/lambdai/gradle/tasks/ThriftCompile.groovy
+++ src/main/groovy/com/lambdai/gradle/tasks/ThriftCompile.groovy
@@ -1,9 +1,14 @@
 package com.lambdai.gradle.tasks
 
 import org.gradle.api.DefaultTask
+import org.gradle.api.tasks.InputFiles
+import org.gradle.api.tasks.OutputDirectory
 
 abstract class ThriftCompile extends DefaultTask {
+    @InputFiles
     def source
+
+    @OutputDirectory
     File output
 
     abstract def compile();
--- src/main/groovy/com/lambdai/gradle/tasks/ThriftScalaCompile.groovy
+++ src/main/groovy/com/lambdai/gradle/tasks/ThriftScalaCompile.groovy
@@ -2,9 +2,13 @@ package com.lambdai.gradle.tasks
 
 import com.lambdai.gradle.tasks.ThriftCompile
 import com.lambdai.gradle.tasks.thrift.ScroogeCompiler
+import org.gradle.api.tasks.Input
+import org.gradle.api.tasks.Optional
 import org.gradle.api.tasks.TaskAction
 
 class ThriftScalaCompile extends ThriftCompile {
+    @Input
+    @Optional
     def opts = ["--finagle"]
 
     @TaskAction
