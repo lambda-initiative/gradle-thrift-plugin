a90fe5cb5b97b3b1a6c223c3951dc386c81b232b
Fuyao Zhao <zfy0701@gmail.com>
2016-03-15 18:33:00 -0700

    upgradle to support gradle 2.12

--- src/main/groovy/com/lambdalab/gradle/DefaultThriftSourceSet.java
+++ src/main/groovy/com/lambdalab/gradle/DefaultThriftSourceSet.java
@@ -4,13 +4,15 @@ import groovy.lang.Closure;
 import org.gradle.api.file.SourceDirectorySet;
 import org.gradle.api.internal.file.DefaultSourceDirectorySet;
 import org.gradle.api.internal.file.FileResolver;
+import org.gradle.api.internal.file.collections.DefaultDirectoryFileTreeFactory;
 import org.gradle.util.ConfigureUtil;
 
 public class DefaultThriftSourceSet implements ThriftSourceSet {
     private final SourceDirectorySet thrift;
 
     public DefaultThriftSourceSet(String displayName, FileResolver fileResolver) {
-        thrift = new DefaultSourceDirectorySet(String.format("%s Thrift source", displayName), fileResolver);
+        thrift = new DefaultSourceDirectorySet(String.format("%s Thrift source", displayName), fileResolver,
+            new DefaultDirectoryFileTreeFactory());
         thrift.getFilter().include("**/*.thrift");
     }
 
